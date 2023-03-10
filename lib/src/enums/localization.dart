cb559d0493dc4700abfe56450722831f30fac799
abdullah harits <harits.abdullah19@gmail.com>
2023-03-10 20:34:03 +0700

    update localization

--- lib/src/enums/localization.dart
+++ lib/src/enums/localization.dart
@@ -1,25 +1,24 @@
-
-
 import 'package:daily_routine_app/src/enums/my_language.dart';
 
 enum Localization {
   version(en: "Version", id: "Versi"),
+  januari(),
   hello(en: "Hello", id: "Hallo");
 
   const Localization({
-    required String en,
-    required String id,
+    String? en,
+    String? id,
   })  : _en = en,
         _id = id;
-  final String _id;
-  final String _en;
+  final String? _id;
+  final String? _en;
 
   String text(MyLanguage myLocal) {
     switch (myLocal) {
       case MyLanguage.en:
-        return _en;
+        return _en ?? name;
       case MyLanguage.id:
-        return _id;
+        return _id ?? name;
     }
   }
 }
